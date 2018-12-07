50091d4a9702a0ccb57ebf68f60afa5af4e697e4
MiChel Causey <michel.causey@gmail.com>
2018-12-06 20:56:04 -0500

    updates

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,23 +2,23 @@ var db = require("../models");
 
 module.exports = function(app) {
   // Get all patient info
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+  app.get("/api/patient", function(req, res) {
+    db.Patient.findAll({}).then(function(dbPatient) {
+      res.json(dbPatient);
     });
   });
 
   // Create a new patient
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  app.post("/api/patient", function(req, res) {
+    db.Patient.create(req.body).then(function(dbPatient) {
+      res.json(dbPatient);
     });
   });
 
   // close patient case -- use "app.update"
   app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
+    db.Patient.destroy({ where: { id: req.params.id } }).then(function(dbPatient) {
+      res.json(dbPatient);
     });
   });
 };
